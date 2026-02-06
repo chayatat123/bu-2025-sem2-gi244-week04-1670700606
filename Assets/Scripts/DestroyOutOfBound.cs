adbb89e5b7ddd3794e37eeabc6210513941b0321
chayatat123 <chayatat.seer@bumail.net>
2026-02-06 16:48:50 +0700

    kak

--- Assets/Scripts/DestroyOutOfBound.cs
+++ Assets/Scripts/DestroyOutOfBound.cs
@@ -2,9 +2,22 @@ using UnityEngine;
 
 public class DestroyOutOfBound : MonoBehaviour
 {
+    public float xRange = 0f;
     // Update is called once per frame
     void Update()
     {
-
+        if(transform.position.z > 20)
+        {
+            Destroy(gameObject);
+            
+        }
+        else if (transform.position.z < -10) 
+        {
+            Destroy(gameObject);
+           
+        }
+        
+            
+     
     }
 }
